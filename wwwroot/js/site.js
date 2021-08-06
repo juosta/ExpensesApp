8afa4a88970bb49cc180a8cceb61a40390814445
juosta <79445286+juosta@users.noreply.github.com>
2021-08-06 10:29:53 +0300

    transaction tabs

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -3,33 +3,21 @@
 
 // Write your JavaScript code.
 
-const allTabs = document.querySelectorAll('.tab');
 
-function activeTab(tab, list) {
-    for (const item in list) {
-        item.classList.remove('show');
-    }
-    tab.classList.add('show');
+// Show/hide tabs in home transactions list
+const allTabBtns = document.querySelectorAll('[id^="tab-btn"]');
+for (const tabBtn of allTabBtns) {
+    tabBtn.addEventListener('click', () => {
+        const allTabs = document.querySelectorAll('.tab');
+        for (const item of allTabBtns) {
+            item.classList.remove('active');
+        }
+        tabBtn.classList.add('active');
+        for (const item of allTabs) {
+            item.classList.remove('show');
+        }
+        const whichToShow = tabBtn.id.substr(-1);
+        const showTab = document.querySelector(`#tab-${whichToShow}`);
+        showTab.classList.add('show');
+    });
 }
-const tabbtn1 = document.querySelector("#tab-btn-1");
-const tabbtn2 = document.querySelector("#tab-btn-2");
-const tab1 = document.querySelector("#tab-1");
-
-const tab2 = document.querySelector("#tab-btn-2");
-tabbtn1.addEventListener('click', () => {
-    const allTabs = document.querySelectorAll('.tab');
-    for (const item of allTabs) {
-        item.classList.remove('show');
-    }
-    const tab1 = document.querySelector("#tab-1");
-    tab1.classList.add('show');
-});
-tabbtn2.addEventListener('click', () => {
-    const allTabs = document.querySelectorAll('.tab');
-    for (const item of allTabs) {
-        item.classList.remove('show');
-    }
-    const tab2 = document.querySelector("#tab-2");
-    tab2.classList.add('show');
-});
-
