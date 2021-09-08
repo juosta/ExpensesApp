7413f69575836a3c5b932a4b291d7533e7787305
juosta <79445286+juosta@users.noreply.github.com>
2021-09-08 19:12:40 +0300

    moved loadChartData into separate file

---
+++ wwwroot/js/chart/loadChartData.js
@@ -0,0 +1,54 @@
+﻿function loadChartData(filterDateFrom, filterDateTo) {
+    $.ajax({
+        url: '/api/Category/GetCategoriesData/?filterDateFrom=' + filterDateFrom + '&filterDateTo=' + filterDateTo,
+        type: 'GET',
+        dataType: 'JSON',
+        success: function (result) {
+            let chartData = [];
+            for (let [key, value] of Object.entries(result)) {
+                chartData[key] = {
+                    name: value.name,
+                    y: value.sum
+                };
+            }
+            console.log(chartData);
+            Highcharts.chart('pieChart', {
+                chart: {
+                    plotBackgroundColor: null,
+                    plotBorderWidth: null,
+                    plotShadow: false,
+                    type: 'pie'
+                },
+                title: {
+                    text: `Expenses per category, from ${filterDateFrom} to ${filterDateTo}`
+                },
+                tooltip: {
+                    pointFormat: '{series.name}: <b>{point.y} Eur</b>'
+                },
+                accessibility: {
+                    point: {
+                        valueSuffix: '%'
+                    }
+                },
+                plotOptions: {
+                    pie: {
+                        allowPointSelect: true,
+                        cursor: 'pointer',
+                        dataLabels: {
+                            enabled: true,
+                            format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+                        }
+                    }
+                },
+                series: [{
+                    name: 'Amount',
+                    colorByPoint: true,
+                    data: chartData
+                }]
+            });
+        },
+        error: function (xhr) {
+            //$.notify("Error", "error");
+        }
+    });
+}
